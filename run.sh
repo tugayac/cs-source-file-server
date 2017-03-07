3fae57a85519ce2ed7758acf444703bda1a933a6
Arda Tugay <tugayac@users.noreply.github.com>
2017-03-06 23:13:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 8089:8089 -v `pwd`/public:/usr/src/app/public cs-source-file-server
+docker run -d -p 8089:8089 -v `pwd`/public:/usr/src/app/public tugayac/cs-source-file-server
